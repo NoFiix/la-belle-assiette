1cced60cf647924fa7a50692c391b6c41654a623
NoFiix <khuddan@gmail.com>
2026-03-20 00:48:31 +0100

    fix: lib/prisma.ts compatible Prisma 7 + adapter pg

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,19 +1,22 @@
-import { PrismaClient } from "@prisma/client";
-import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from '@prisma/client'
+import { PrismaPg } from '@prisma/adapter-pg'
 
 function createPrismaClient() {
-  const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL! });
-  return new PrismaClient({ adapter });
+  const connectionString = new URL(process.env.DIRECT_URL!)
+  connectionString.searchParams.set('uselibpqcompat', 'true')
+
+  const adapter = new PrismaPg({ connectionString: connectionString.toString() })
+  return new PrismaClient({ adapter })
 }
 
 const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined;
-};
-
-const prisma = globalForPrisma.prisma ?? createPrismaClient();
-
-if (process.env.NODE_ENV !== "production") {
-  globalForPrisma.prisma = prisma;
+  prisma: PrismaClient | undefined
 }
 
-export default prisma;
+const prisma = globalForPrisma.prisma ?? createPrismaClient()
+
+if (process.env.NODE_ENV !== 'production') {
+  globalForPrisma.prisma = prisma
+}
+
+export default prisma
